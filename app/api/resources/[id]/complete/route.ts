504993b5caff7187fa0000b781399bf920879b1d
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2026-04-16 19:18:25 -0400

    feat: add /resource/[id]/complete

--- app/api/resources/[id]/complete/route.ts
+++ app/api/resources/[id]/complete/route.ts
@@ -1,28 +1,21 @@
 import { getCurrentUser } from "@/src/lib/auth";
 import { NextResponse } from "next/server";
 import { completeResource } from "./completed.service";
-import { completeResourceSchema } from "../../dtos/complete-resource.dto";
+import { completeResourceSchema } from "../../../dtos/complete-resource.dto";
 import { unCompleteResource } from "./completed.service";
 
-export async function POST(request: Request) {
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
   try {
     const user = await getCurrentUser();
 
     if (!user?.sub) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-
-    const body = await request.json();
-    const parsed = completeResourceSchema.safeParse(body);
-
-    if (!parsed.success) {
-      return NextResponse.json(
-        { error: parsed.error.flatten() },
-        { status: 400 },
-      );
-    }
-
-    await completeResource(user.sub, parsed.data.resourceId);
+    await completeResource(user.sub, id);
 
     return NextResponse.json({ message: "Resource marked as completed" });
   } catch {
@@ -33,24 +26,15 @@ export async function POST(request: Request) {
   }
 }
 
-export async function DELETE(request: Request) {
+export async function DELETE({ params }: { params: { id: string } }) {
   try {
     const user = await getCurrentUser();
 
     if (!user?.sub) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    const body = await request.json();
-    const parsed = completeResourceSchema.safeParse(body);
 
-    if (!parsed.success) {
-      return NextResponse.json(
-        { error: parsed.error.flatten() },
-        { status: 400 },
-      );
-    }
-
-    await unCompleteResource(user.sub, parsed.data.resourceId);
+    await unCompleteResource(user.sub, params.id);
 
     return NextResponse.json({ message: "Resource marked as incomplete" });
   } catch {
