0bcee154d4ee2e49e585b49748afe8e4f8fb92b1
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2026-04-17 23:14:58 -0400

    chore: remove branch

--- app/api/talleres/route.ts
+++ app/api/talleres/route.ts
@@ -2,16 +2,13 @@ const GITHUB_API =
   "https://api.github.com/repos/Victorsitou/licc-ramos/contents";
 
 export async function GET() {
-  const talleres = await fetch(
-    `${GITHUB_API}/public/Talleres?ref=feat/talleres`,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-        Accept: "application/vnd.github.raw+json",
-        "X-GitHub-Api-Version": "2026-03-10",
-      },
+  const talleres = await fetch(`${GITHUB_API}/public/Talleres`, {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+      Accept: "application/vnd.github.raw+json",
+      "X-GitHub-Api-Version": "2026-03-10",
     },
-  ).then((r) => r.json());
+  }).then((r) => r.json());
 
   const response = [];
 
