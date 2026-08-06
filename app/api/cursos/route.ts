1bb5679107b3e95c4c72a3bcfe35cb80724c9ac0
Victorsitou <67214928+Victorsitou@users.noreply.github.com>
2026-08-06 16:02:14 -0400

    feat: load historic courses

--- app/api/cursos/route.ts
+++ app/api/cursos/route.ts
@@ -6,7 +6,6 @@ export async function GET() {
     const archivos = await fs.readdir(process.cwd() + "/app/cursos", {
       withFileTypes: true,
     });
-
     const archivosFiltrados = archivos
       .filter((archivo) => archivo.isFile() && archivo.name.endsWith(".json"))
       .map((archivo) => archivo.name);
@@ -20,6 +19,34 @@ export async function GET() {
       );
       const jsonData = JSON.parse(data);
       ramos.push(jsonData);
+    }
+
+    // Recursivo
+    const archivosRecursivos = await fs.readdir(process.cwd() + "/app/cursos", {
+      withFileTypes: true,
+    });
+
+    for (const archivo of archivosRecursivos) {
+      if (archivo.isDirectory()) {
+        const subArchivos = await fs.readdir(
+          process.cwd() + "/app/cursos/" + archivo.name,
+          { withFileTypes: true },
+        );
+        for (const subArchivo of subArchivos) {
+          if (subArchivo.isFile() && subArchivo.name.endsWith(".json")) {
+            const data = await fs.readFile(
+              process.cwd() +
+                "/app/cursos/" +
+                archivo.name +
+                "/" +
+                subArchivo.name,
+              "utf-8",
+            );
+            const jsonData = JSON.parse(data);
+            ramos.push(jsonData);
+          }
+        }
+      }
     }
 
     return NextResponse.json(ramos);
